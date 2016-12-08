7422f7b3e895b9377e21f43c4dff47339765fc16
gabrielodom <gabriel_odom@baylor.edu>
2016-12-08 16:08:58 -0600

    Test Commit

--- R/processMonitor.R
+++ R/processMonitor.R
@@ -69,6 +69,7 @@ processMonitor <- function(data,
     nonSPEFlaggedObs <- faultObj[faultObj$SPE_flag == FALSE, ]
     nonFlaggedObs <- nonSPEFlaggedObs[nonSPEFlaggedObs$T2_flag == FALSE, ]
     unflaggedObs[(nrow(unflaggedObs) + 1):(nrow(unflaggedObs) + nrow(nonFlaggedObs)),] <- data[rownames(data) == rownames(nonFlaggedObs),]
+    12
   }
 
 }
