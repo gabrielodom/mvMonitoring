e95639c9553448b60f0f0daf5bea683486168f0a
gabrielodom <gabriel_odom@baylor.edu>
2016-12-09 18:29:27 -0600

    Piecing out the processMonitor function's inner while() loop as a called function.

---
+++ R/faultFilter.R
@@ -0,0 +1,58 @@
+
+
+#' Title
+#'
+#' @param trainingData
+#' @param updateFreq
+#' @param testingData
+#' @param ...
+#'
+#' @return
+#' @exportfrom lazyeval lazy_eval
+#'
+#' @examples
+faultFilter <- function(trainingData, testingData, updateFreq, ...){
+  UseMethod("faultFilter")
+}
+
+faultFilter.training <- function(trainingData,
+                                 testingData,
+                                 updateFreq,
+                                 ...){
+  browser()
+  muTrain <- colMeans(trainData)
+  sigmaTrain <- cov(trainData)
+  sigmaInvTrain <- solve(sigmaTrain)
+
+  scaledTrainData <- scale(trainData)
+
+
+  pcaObj <- do.call(pca, args = c(list(data = scaledTrainData), lazy_eval(ls)))
+  thresholdObj <- do.call(threshold, args = c(list(pca_object = pcaObj),
+                                              lazy_eval(ls)))
+  scaledTest <- as.matrix(testingData - muTrain) %*% sigmaInvTrain
+  faultObj <- lapply(i in 1:nrow(scaledTest), function(i){
+    do.call(faultDetect,
+            args = c(list(threshold_object = thresholdObj,
+                          observation = scaledObs[i,]),
+                     lazy_eval(ls)))
+  })
+
+  nonflaggedSPEObs <- nrow(faultObj[faultObj$SPE_flag == FALSE,])
+  continueSPE <- nonflaggedSPEObs < updateFreq
+  nonflaggedT2Obs <- nrow(faultObj[faultObj$T2_flag == FALSE,])
+  continueT2 <- nonflaggedT2Obs < updateFreq
+
+  if(nrow(faultObj) >= 3 && all(faultObj[(iter - 2):iter, 2]) == TRUE){
+    warning("SPE detects process fault.", immediate. = TRUE)
+  }
+  if(nrow(faultObj) >= 3 && all(faultObj[(iter - 2):iter, 4]) == TRUE){
+    warning("T2 detects process fault.", immediate. = TRUE)
+  }
+
+
+  if(trainObs + iter == nrow(data)){
+    print("End of Data Frame Reached")
+    break()
+  }
+}
--- R/processMonitor.R
+++ R/processMonitor.R
@@ -34,45 +34,13 @@ processMonitor <- function(data,
       }else{
         trainData <- unflaggedObs[(n - trainObs):n,]
         }
-    muTrain <- colMeans(trainData)
-    sigmaTrain <- cov(trainData)
-    sigmaInvTrain <- solve(sigmaTrain)
-    scaledTrainData <- scale(trainData)
 
     iter <- 1 + nrow(faultObj)
     continueSPE <- TRUE
     continueT2 <- TRUE
 
     while(continueSPE && continueT2){
-      pcaObj <- do.call(pca,
-                        args = c(list(data = scaledTrainData), lazy_eval(ls)))
-      thresholdObj <- do.call(threshold,
-                              args = c(list(pca_object = pcaObj),
-                                       lazy_eval(ls)))
-      scaledObs <- sigmaInvTrain %*%
-                      t(as.matrix(data[(trainObs + iter),]) - muTrain)
-      faultObj[iter,] <- do.call(faultDetect,
-                                 args = c(list(threshold_object = thresholdObj,
-                                               observation = t(scaledObs)),
-                                          lazy_eval(ls)))
 
-      nonflaggedSPEObs <- nrow(faultObj[faultObj$SPE_flag == FALSE,])
-      continueSPE <- nonflaggedSPEObs < updateFreq
-      nonflaggedT2Obs <- nrow(faultObj[faultObj$T2_flag == FALSE,])
-      continueT2 <- nonflaggedT2Obs < updateFreq
-
-      if(nrow(faultObj) >= 3 && all(faultObj[(iter - 2):iter, 2]) == TRUE){
-        warning("SPE detects process fault.", immediate. = TRUE)
-      }
-      if(nrow(faultObj) >= 3 && all(faultObj[(iter - 2):iter, 4]) == TRUE){
-        warning("T2 detects process fault.", immediate. = TRUE)
-      }
-
-
-      if(trainObs + iter == nrow(data)){
-        print("End of Data Frame Reached")
-        break()
-      }
       iter <- iter + 1
     }
     rownames(faultObj) <-
