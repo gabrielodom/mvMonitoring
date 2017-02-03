c816f0593bc957f3ba8004d7e0e42521c3d73d73
gabrielodom <gabriel_odom@baylor.edu>
2017-02-02 18:32:46 -0600

    Fixed list presentation for the fault data files. Completed Dr. Hering's edits for the NOC data file.

--- R/fault2A_xts.R
+++ R/fault2A_xts.R
@@ -10,21 +10,21 @@
 #'   fault is a drift on each feature by s / 10 ^ 3, where s is the observation
 #'   index. The fault starts at row 8500, and the four columns under the fault
 #'   state are defined here:
-#' \describe{
-#'   \item{state}{the state indicator for the multivariate system, with three
+#' \itemize{
+#'   \item{state : }{the state indicator for the multivariate system, with three
 #'   levels}
-#'   \item{x}{x(t) = t + drift + error}
-#'   \item{y}{y(t) = t ^ 2 - 3t + drift + error}
-#'   \item{z}{z(t) = - t ^ 3 + 3t ^ 2 + drift + error}
+#'   \item{x : }{x(t) = t + drift + error}
+#'   \item{y : }{y(t) = t ^ 2 - 3t + drift + error}
+#'   \item{z : }{z(t) = - t ^ 3 + 3t ^ 2 + drift + error}
 #' }
 #' where t is a 10080-entry vector of autocorrelated and non-stationary hidden
 #' process realizations. The states alternate each hour and are defined as
 #' follows:
-#' \describe{
-#'   \item{State1}{As presented}
-#'   \item{State2}{Rotated by (yaw = 0, pitch = 90, roll = 30) and scaled by
+#' \itemize{
+#'   \item{State1 -- }{As presented}
+#'   \item{State2 -- }{Rotated by (yaw = 0, pitch = 90, roll = 30) and scaled by
 #'   (1 * x, 0.5 * y , 2 * z).}
-#'   \item{State3}{Rotated by (yaw = 90, pitch = 0, roll = -30) and scaled by
+#'   \item{State3 -- }{Rotated by (yaw = 90, pitch = 0, roll = -30) and scaled by
 #'   (0.25 * x, 0.1 * y , 0.75 * z).}
 #' }
 #' See the vignette for more details.
