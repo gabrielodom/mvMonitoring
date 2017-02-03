31cfad41983070b8b4712d39a6573dc8b2531e9f
gabrielodom <gabriel_odom@baylor.edu>
2017-02-02 21:28:47 -0600

    Added data from K. Newhart. Edited the oneDay and nineDay files. The source data is a ten day file an a 12 hour file, but the 12 hour file is from the start of the training interval, so it is useless to test with. The new oneDay file is the last day of the original tenDay file, and we will use it to test the package. We thus trimmed the last daty from the training data, and saved it as nineDay.

---
+++ R/nineDay_xts.R
@@ -0,0 +1,10 @@
+#'  Real Process Data for Training
+#'
+#' @description Data from the SM-MBR Bioreactor system over nine days. This data
+#'   will be used for training the mvMonitoring package.
+#'
+#' @format An xts matrix of 1,293 rows and 36 features recorded over
+#'   2017-01-17 at 00:10 to 2017-01-26 at 00:00.
+#'
+#' @source Kathryn Newhart
+"nineDay"
---
+++ R/oneDay_xts.R
@@ -0,0 +1,10 @@
+#'  Real Process Data for Testing
+#'
+#' @description Data from the SM-MBR Bioreactor system over 24 hours. This data
+#'   will be used for testing the mvMonitoring package.
+#'
+#' @format An xts matrix of 144 rows and 36 features recorded over
+#'   2017-01-26 at 00:10 to 2017-01-27 at 00:00.
+#'
+#' @source Kathryn Newhart
+"oneDay"
