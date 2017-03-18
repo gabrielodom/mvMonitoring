7f1459fb4bbc3efeb711370c28563fda257370c7
gabrielodom <gabriel_odom@baylor.edu>
2017-03-17 19:55:02 -0500

    Completed Dr. Hering's requested figure modifications.

--- inst/mspGraphsGrid.R
+++ inst/mspGraphsGrid.R
@@ -24,57 +24,75 @@ faultStart <- index(adpcaObs_ls$NOC[8500,])
 
 ######  Create Graphing Function  ######
 mspGraphsGrid <- function(data_xts){
-  obs_df <- data_xts[7500:10080, 1:4] %>% as.data.frame() # 8461
-  obs_df$dateTime <- index(data_xts[7500:10080,]) # 8461
-  obs_df <- obs_df %>% mutate(state = as.factor(state))
+  # The fault is in the state from 8460:8520 (8500 / 60 = 141.67). Dr. Hering
+  # wants to see four full cycles before this state, and six full cycles after.
+  # Thus, we start at 141 * 60 - 4 * 3 * 60 = 7740 and end at
+  # 142 * 60 + 6 * 3 * 60 = 9600.
+  obs_df <- data_xts[7741:9600, 1:4] %>% as.data.frame()
+  obs_df$dateTime <- index(data_xts[7741:9600,])
+  obs_df <- obs_df %>% mutate(State = as.factor(state))
+
+  # Friendly colours for the Colour-Blind
+  # http://bconnelly.net/2013/10/creating-colorblind-friendly-figures/
+  cbFriend = c(`1` = "#E69F00", `2` = "#56B4E9", `3` = "#009E73")
 
   p1 <- ggplot(data = obs_df,
-               aes(x = dateTime, y = x, color = state, group = 1)) +
+               aes(x = dateTime, y = x, color = State, group = 1)) +
     # group = 1 in the aesthetic means that you want different colours, but not
     # different groups. See
     # https://kohske.wordpress.com/2010/12/27/faq-geom_line-doesnt-draw-lines/
-    scale_colour_manual(values = c(`1` = "red", `2` = "green", `3` = "blue"),
-                        breaks = c("1", "2", "3"),
-                        guide = FALSE) +
-    geom_line() +
-    scale_x_datetime(breaks = c(as.POSIXct("2016-12-02 12:00:00 CST"),
-                                as.POSIXct("2016-12-03 12:00:00 CST")),
-                     date_labels = "%m-%d %H:%M") +
-    theme_bw() +
-    theme(axis.title.y = element_blank(),
-          plot.title = element_text(hjust = 0.5)) +
-    ggtitle("Feature X") +
-    geom_vline(xintercept = as.numeric(faultStart), size = 1.2)
-
-  p2 <- ggplot(data = obs_df,
-               aes(x = dateTime, y = y, color = state, group = 1)) +
-    scale_colour_manual(values = c(`1` = "red", `2` = "green", `3` = "blue"),
-                        breaks = c("1", "2", "3"),
-                        guide = FALSE) +
-    geom_line() +
-    scale_x_datetime(breaks = c(as.POSIXct("2016-12-02 12:00:00 CST"),
-                                as.POSIXct("2016-12-03 12:00:00 CST")),
-                     date_labels = "%m-%d %H:%M") +
-    theme_bw() +
-    theme(axis.title.y = element_blank(),
-          plot.title = element_text(hjust = 0.5)) +
-    ggtitle("Feature Y") +
-    geom_vline(xintercept = as.numeric(faultStart), size = 1.2)
-
-  p3 <- ggplot(data = obs_df,
-               aes(x = dateTime, y = z, color = state, group = 1)) +
-    scale_colour_manual(values = c(`1` = "red", `2` = "green", `3` = "blue"),
+    scale_colour_manual(values = cbFriend,
                         breaks = c("1", "2", "3")) +
     geom_line() +
     scale_x_datetime(breaks = c(as.POSIXct("2016-12-02 12:00:00 CST"),
                                 as.POSIXct("2016-12-03 12:00:00 CST")),
-                     date_labels = "%m-%d %H:%M") +
+                     date_labels = "%m-%d %H:%M",
+                     name = "") +
+    scale_y_continuous(limits = c(-0.5, 3.5)) +
     theme_bw() +
-    theme(axis.title.y = element_blank(),
+    theme(panel.grid = element_blank(),
+          axis.title.y = element_blank(),
           plot.title = element_text(hjust = 0.5),
-          legend.justification=c(1,0), legend.position=c(1,0)) +
+          legend.position = c(0.12, 0.85)) + # legend.justification = c(1,0),
+    guides(color = guide_legend(override.aes = list(size = 2))) +
+    ggtitle("Feature X") +
+    geom_vline(xintercept = as.numeric(faultStart), size = 1)
+
+  p2 <- ggplot(data = obs_df,
+               aes(x = dateTime, y = y, color = State, group = 1)) +
+    scale_colour_manual(values = cbFriend,
+                        breaks = c("1", "2", "3"),
+                        guide = FALSE) +
+    geom_line() +
+    scale_x_datetime(breaks = c(as.POSIXct("2016-12-02 12:00:00 CST"),
+                                as.POSIXct("2016-12-03 12:00:00 CST")),
+                     date_labels = "%m-%d %H:%M",
+                     name = "Time") +
+    scale_y_continuous(limits = c(-2.5, 2)) +
+    theme_bw() +
+    theme(panel.grid = element_blank(),
+          axis.title.y = element_blank(),
+          plot.title = element_text(hjust = 0.5)) +
+    ggtitle("Feature Y") +
+    geom_vline(xintercept = as.numeric(faultStart), size = 1)
+
+  p3 <- ggplot(data = obs_df,
+               aes(x = dateTime, y = z, color = State, group = 1)) +
+    scale_colour_manual(values = cbFriend,
+                        breaks = c("1", "2", "3"),
+                        guide = FALSE) +
+    geom_line() +
+    scale_x_datetime(breaks = c(as.POSIXct("2016-12-02 12:00:00 CST"),
+                                as.POSIXct("2016-12-03 12:00:00 CST")),
+                     date_labels = "%m-%d %H:%M",
+                     name = "") +
+    scale_y_continuous(limits = c(-6, 3)) +
+    theme_bw() +
+    theme(panel.grid = element_blank(),
+          axis.title.y = element_blank(),
+          plot.title = element_text(hjust = 0.5)) +
     ggtitle("Feature Z") +
-    geom_vline(xintercept = as.numeric(faultStart), size = 1.2)
+    geom_vline(xintercept = as.numeric(faultStart), size = 1)
 
   grid.arrange(p1, p2, p3, ncol = 3)
 }
