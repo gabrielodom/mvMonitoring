c0c22209082a6c52e654625a695814e751cc28f4
Gabriel J. Odom <gjo15@med.miami.edu>
2018-01-25 15:17:14 -0500

    Reminder, the code to build the simulation graphs in the paper is in inst/.

--- inst/mspGraphsGrid.R
+++ inst/mspGraphsGrid.R
@@ -24,13 +24,18 @@ adpcaObs_ls %>% str
 faultStart <- index(adpcaObs_ls$NOC[8500,])
 
 ######  Create Graphing Function  ######
-mspGraphsGrid <- function(data_xts){
+mspGraphsGrid <- function(data_xts,
+                          rows = 7741:9600,
+                          faultStart = "2016-12-02 21:39:00 EST",
+                          Xlims = c(-0.5, 3.5),
+                          Ylims = c(-2.5, 2),
+                          Zlims = c(-6, 3)){
   # The fault is in the state from 8460:8520 (8500 / 60 = 141.67). Dr. Hering
   # wants to see four full cycles before this state, and six full cycles after.
   # Thus, we start at 141 * 60 - 4 * 3 * 60 = 7740 and end at
   # 142 * 60 + 6 * 3 * 60 = 9600.
-  obs_df <- data_xts[7741:9600, 1:4] %>% as.data.frame()
-  obs_df$dateTime <- index(data_xts[7741:9600,])
+  obs_df <- data_xts[rows, 1:4] %>% as.data.frame()
+  obs_df$dateTime <- index(data_xts[rows,])
   obs_df <- obs_df %>% mutate(State = as.factor(state))
 
   # Friendly colours for the Colour-Blind
@@ -49,7 +54,7 @@ mspGraphsGrid <- function(data_xts){
                                 as.POSIXct("2016-12-03 12:00:00 CST")),
                      date_labels = "%m-%d %H:%M",
                      name = "") +
-    scale_y_continuous(limits = c(-0.5, 3.5)) +
+    scale_y_continuous(limits = Xlims) +
     theme_bw() +
     theme(panel.grid = element_blank(),
           axis.title.y = element_blank(),
@@ -58,7 +63,7 @@ mspGraphsGrid <- function(data_xts){
           legend.background = element_blank()) + # legend.justification = c(1,0),
     guides(color = guide_legend(override.aes = list(size = 2))) +
     ggtitle("Feature X") +
-    geom_vline(xintercept = as.numeric(faultStart), size = 1)
+    geom_vline(xintercept = as.numeric(as.POSIXct(faultStart)), size = 1)
 
   p2 <- ggplot(data = obs_df,
                aes(x = dateTime, y = y, color = State, group = 1)) +
@@ -70,13 +75,13 @@ mspGraphsGrid <- function(data_xts){
                                 as.POSIXct("2016-12-03 12:00:00 CST")),
                      date_labels = "%m-%d %H:%M",
                      name = "Time") +
-    scale_y_continuous(limits = c(-2.5, 2)) +
+    scale_y_continuous(limits = Ylims) +
     theme_bw() +
     theme(panel.grid = element_blank(),
           axis.title.y = element_blank(),
           plot.title = element_text(hjust = 0.5)) +
     ggtitle("Feature Y") +
-    geom_vline(xintercept = as.numeric(faultStart), size = 1)
+    geom_vline(xintercept = as.numeric(as.POSIXct(faultStart)), size = 1)
 
   p3 <- ggplot(data = obs_df,
                aes(x = dateTime, y = z, color = State, group = 1)) +
@@ -88,7 +93,7 @@ mspGraphsGrid <- function(data_xts){
                                 as.POSIXct("2016-12-03 12:00:00 CST")),
                      date_labels = "%m-%d %H:%M",
                      name = "") +
-    scale_y_continuous(limits = c(-6, 3)) +
+    scale_y_continuous(limits = Zlims) +
     theme_bw() +
     theme(panel.grid = element_blank(),
           axis.title.y = element_blank(),
@@ -96,14 +101,14 @@ mspGraphsGrid <- function(data_xts){
           # So that the right side of Figure Z doesn't get eaten by the PDF
           plot.margin = unit(c(0.2,0.5,0.2,0.2), "cm")) +
     ggtitle("Feature Z") +
-    geom_vline(xintercept = as.numeric(faultStart), size = 1)
+    geom_vline(xintercept = as.numeric(as.POSIXct(faultStart)), size = 1)
 
   grid.arrange(p1, p2, p3, ncol = 3)
 }
 
 
 ######  Use Graphing Function  ######
-setwd("C:/Users/gabriel_odom/Box Sync/Consulting/Dr. Hering/MV_Process_Control/MVSPC/Paper Graphs")
+setwd("C:/Users/gabriel_odom/Box Sync/Consulting/Dr. Hering/MV_Process_Control/MVSPC (including Graphics)/Paper Graphs")
 
 # Getting the PDFs to look exactly the way we need them to in the LaTeX document
 # can be a giant pain in the ass.
@@ -146,4 +151,21 @@ dev.off()
 
 pdf("Fault3C_20170420.pdf", width = 8.624, height = 3.744)
 adpcaObs_ls$C3 %>% mspGraphsGrid()
+dev.off()
+
+
+######  Graphs for the Reviewer  ######
+# After saving, move files to:
+# ~/Box Sync/Consulting/Dr. Hering/MV_Process_Control/MVSPC (including Graphics)/Paper Graphs
+
+pdf("NOC_20180125.pdf", width = 8.624, height = 3.744)
+adpcaObs_ls$NOC %>% mspGraphsGrid(rows = 1:10080,
+                                 Xlims = c(-0.5, 4.5),
+                                 Zlims = c(-6, 5))
+dev.off()
+
+pdf("Fault3C_20180125.pdf", width = 8.624, height = 3.744)
+adpcaObs_ls$C3 %>% mspGraphsGrid(rows = 1:10080,
+                                 Xlims = c(-0.5, 4.5),
+                                 Zlims = c(-6, 5))
 dev.off()
